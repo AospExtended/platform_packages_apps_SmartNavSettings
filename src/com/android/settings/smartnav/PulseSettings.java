000f6af7031bc9b2980c39b58af6463b80435582
Shubham Singh <coolsks94@gmail.com>
2019-01-06 01:39:22 +0530

    Extensions: Add extensions frags to search index [2/3]
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Change-Id: If945ab8769bfa84d7286359541b0e3c8a8c6d6e5

--- src/com/android/settings/smartnav/PulseSettings.java
+++ src/com/android/settings/smartnav/PulseSettings.java
@@ -16,6 +16,7 @@
 
 package com.android.settings.smartnav;
 
+import android.content.Context;
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -29,12 +30,19 @@ import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.ListPreference;
 import android.provider.Settings;
+import android.provider.SearchIndexableResource;
 
 import net.margaritov.preference.colorpicker.ColorPickerPreference;
 import org.aospextended.extensions.preference.CustomSeekBarPreference;
 
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PulseSettings extends SettingsPreferenceFragment implements
-        Preference.OnPreferenceChangeListener {
+        Preference.OnPreferenceChangeListener, Indexable {
 
     private static final String TAG = PulseSettings.class.getSimpleName();
     private static final String CUSTOM_DIMEN = "pulse_custom_dimen";
@@ -278,4 +286,22 @@ public class PulseSettings extends SettingsPreferenceFragment implements
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.EXTENSIONS;
     }
+
+    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+                 @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                        boolean enabled) {
+                    final ArrayList<SearchIndexableResource> result = new ArrayList<>();
+                     final SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.pulse_settings;
+                    result.add(sir);
+                    return result;
+                }
+                 @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    final List<String> keys = super.getNonIndexableKeys(context);
+                    return keys;
+                }
+    };
 }
